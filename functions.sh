5ce4a8b84daa6757da826d7709a5a6ed77640f48
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-30 20:58:25 +0200

    Agrego cabecera

--- functions.sh
+++ functions.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
+##
+## @author     Raúl Caro Pastorino
+## @copyright  Copyright © 2018 Raúl Caro Pastorino
+## @license    https://wwww.gnu.org/licenses/gpl.txt
+## @email      tecnico@fryntiz.es
+## @web        www.fryntiz.es
+## @github     https://github.com/fryntiz
+## @gitlab     https://gitlab.com/fryntiz
+## @twitter    https://twitter.com/fryntiz
+##
+##             Guía de estilos aplicada:
+## @style      https://github.com/fryntiz/Bash_Style_Guide
 
-DIR_APACHE='/var/www/html/Publico'
+############################
+##     INSTRUCCIONES      ##
+############################
+## Funciones de apoyo
 
 agregar_DNS() {
 

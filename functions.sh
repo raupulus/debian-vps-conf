6c9f19c184aad5d6e1fd38be2ccb3997f10944d6
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-06-30 20:48:38 +0200

    Planteo funciones

--- functions.sh
+++ functions.sh
@@ -1,5 +1,41 @@
 #!/usr/bin/env bash
 # -*- ENCODING: UTF-8 -*-
 
+DIR_APACHE='/var/www/html/Publico'
+
+agregar_DNS() {
+
+}
+
+gitHub() {
+
+}
+
+
+## llamada:
+## gitLab fryntiz curriculum fryntiz/curriculum
+gitLab() {
+    ## $1 Nombre Usuario
+    ## $2 Nombre Repositorio
+    ## $3 subdirectorio/nombre
+
+    echo "Descargando $2"
+    git clone https://gitlab.com/$1/$2 $DIR_APACHE/$3
+}
+
+agregarConfigApache() {
+
+
+}
+
+agregarConfigHost() {
+
+
+}
+
+
+
+
+
 
 exit 0
